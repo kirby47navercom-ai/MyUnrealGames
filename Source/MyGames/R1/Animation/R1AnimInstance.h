3ca98cc74f929e9365e15c06c861b7c17fa07fff
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2026-07-08 11:14:14 +0900

    애니메이션 기초

---
+++ Source/MyGames/R1/Animation/R1AnimInstance.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "R1AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGAMES_API UR1AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UR1AnimInstance(const FObjectInitializer& Parent = FObjectInitializer::Get());
+	
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+protected:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<class AR1Player> Character;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<class UCharacterMovementComponent> MovementComponent;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	FVector Velocity;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	float GroundSpeed;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	bool bShouldMove;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	bool bIFalling;
+	
+	
+};
