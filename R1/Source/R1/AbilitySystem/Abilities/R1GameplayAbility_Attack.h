7d43091dd9879919c5b35cc1a27f1fff0198d466
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2026-07-14 17:07:22 +0900

    dtd

--- R1/Source/R1/AbilitySystem/Abilities/R1GameplayAbility_Attack.h
+++ R1/Source/R1/AbilitySystem/Abilities/R1GameplayAbility_Attack.h
@@ -13,5 +13,11 @@ UCLASS()
 class R1_API UR1GameplayAbility_Attack : public UR1GameplayAbility
 {
 	GENERATED_BODY()
+public:
+	UR1GameplayAbility_Attack(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 	
+protected:
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* SourceTags,const FGameplayTagContainer* TargetTags,FGameplayTagContainer* OptionalRelevantTags) const override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* SourceTags,const FGameplayEventData* TriggerEventData)override;
+	virtual void EndAbility()override;
 };
